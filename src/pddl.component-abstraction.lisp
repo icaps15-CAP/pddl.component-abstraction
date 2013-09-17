9859548f874db7c850a2a0a88298b521517b7b51
Masataro Asai <guicho2.71828@gmail.com>
2013-09-18 03:36:38 +0900

    the program ends without errors, but the result is incorrect

--- src/pddl.component-abstraction.lisp
+++ src/pddl.component-abstraction.lisp
@@ -14,10 +14,6 @@
 (cl-syntax:use-syntax :annot)
 ;; blah blah blah.
 
-
-(defun build-static-graph ()
-  )
-
 @export
 (defun all-instantiated-types (problem)
   (remove-duplicates (mapcar #'type (objects problem))))
@@ -53,7 +49,7 @@
                 (rcurry #'pddl-supertype-p (type p))
                 (all-instantiated-types *problem*)))
              (parameters pred)))))))
-    
+
 @export
 (defun fluent-predicate-p (predicate)
   (let ((*domain* (domain predicate)))
@@ -79,22 +75,118 @@
              nil))))
 
 @export
-(defun static-predicates (problem)
-  (remove-if #'predicate-ignored-p-JAIR-2415
-             (remove-if #'fluent-predicate-p
-                        (all-instantiated-predicates problem))))
-
-@export
 (defun static-facts (problem)
-  (remove-if #'predicate-ignored-p-JAIR-2415
-             (remove-if #'fluent-predicate-p
-                        (init problem))))
+  (remove-if (disjoin (rcurry #'typep 'pddl-function-state)
+                      #'fluent-predicate-p
+                      #'predicate-ignored-p-JAIR-2415)
+             (init problem)))
 
 @export
-(defun statically-connected-to (static-facts &rest objects)
-  (some (lambda (pred)
-          (every #'pddl-supertype-p
-                 objects
-                 (parameters pred)))
-        static-facts))
+(defun static-predicates (problem)
+  (remove-if (disjoin #'fluent-predicate-p
+                      #'predicate-ignored-p-JAIR-2415
+                      ;; (lambda (pred)
+                      ;;   (some (curry (conjoin #'eqname #'predicate-agrees-p) pred)
+                      ;;         (static-facts problem)))
+                      )
+             (all-instantiated-predicates problem)))
+
+(defstruct abstract-component
+  (facts nil)
+  (components nil))
+
+(defmethod parameters ((ac abstract-component))
+  (abstract-component-components ac))
+
+(defmethod print-object ((ac abstract-component) s)
+  (print-unreadable-object (ac s)
+    (format s "(ABS ~w)" (parameters ac))))
+
+(defun %constants (static-facts)
+  (remove-duplicates
+   (mappend #'parameters static-facts)))
+
+(defun %all-types (constants)
+  (remove-duplicates
+   (mapcar #'type constants)))
+
+@export
+(defun cluster-objects (static-facts
+                        static-predicates)
+  ;; follow the expression by adi literally
+  ;; 全部の順番からやってみて、ダメだったら最初からやり直しというアルゴリズム
+  ;; greedy hill-climbingか
+  (iter (with constants = (%constants static-facts))
+        (for ac = nil)
+        (for open = nil)
+        (for closed = nil)
+        (for tried = nil)
+        (for type in (%all-types constants))
+        (push type open)
+        (iter (for o in (remove-if-not (rcurry #'pddl-typep type) constants))
+              (push (make-abstract-component :components (list o)) ac))
+        ;(break+ ac)
+        (iter (while open)
+              ;(break+ open closed tried)
+              (for t1 = (pop open))
+              (push t1 closed)
+              (iter (for p in (set-difference
+                               (remove-if-not
+                                (lambda (pred)
+                                  (some (rcurry #'pddl-typep t1)
+                                        (parameters pred)))
+                                static-predicates)
+                               tried))
+                    (push p tried)
+                    (for p-facts = (remove-if-not
+                                    (curry (conjoin
+                                            #'eqname
+                                            #'predicate-more-specific-p) p)
+                                    static-facts))
+                    (unless (predicate-connects-components p-facts ac)
+                      (setf ac (extend-components p-facts ac))
+                      (iter (for t2 in (remove-duplicates
+                                        (mapcar #'type (parameters p))))
+                            (unless (or (find t2 open)
+                                        (find t2 closed))
+                              (push t2 open))))))
+        (collect ac)
+        ;; (finding
+        ;;  ac maximizing
+        ;;  (reduce #'max ac :key (lambda (c)
+        ;;                          (length
+        ;;                           (remove-duplicates
+        ;;                            (mapcar #'type (parameters c)))))))
+        ;; (when (decomposition-satisfactory-p 4 ac)
+        ;;   (return-from cluster-objects ac))
+        ))
+
+(defun static-fact-extends-ac-p (f ac)
+  (intersection (parameters f) (parameters ac)))
+
+(defun predicate-connects-components (facts ac)
+  (some
+   (lambda (f)
+     (< 1 (length (remove-if-not (curry #'static-fact-extends-ac-p f) ac))))
+   facts))
+
+(defun extend-components (facts ac)
+  (dolist (f facts)
+    (match (find (curry #'static-fact-extends-ac-p f) ac)
+      ((abstract-component (place facts) (place components))
+       (push f facts)
+       (unionf components (parameters f)))
+      (nil
+       (push (make-abstract-component
+              :facts (list f)
+              :components (parameters f))
+             ac))))
+  ac)
+
+(defun decomposition-satisfactory-p (high ac)
+  (every
+   (lambda (component)
+     (<= 2 (length (remove-duplicates (mapcar #'type (parameters component)))) high))
+   ac))
+
 
