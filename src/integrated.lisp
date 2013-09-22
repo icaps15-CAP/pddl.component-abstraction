99ae3833a21a3c7cdfe6c62d3c740b5399709118
Masataro Asai <guicho2.71828@gmail.com>
2013-09-22 16:51:12 +0900

    use *problem* instead of problem

--- src/integrated.lisp
+++ src/integrated.lisp
@@ -2,15 +2,15 @@
 (cl-syntax:use-syntax :annot)
 
 @export
-(defun abstract-components (problem)
+(defun abstract-components (*problem*)
   (mapcar (rcurry #'categorize-by-equality #'abstract-type<=>)
-          (cluster-objects (static-facts problem)
-                           (static-predicates problem))))
+          (cluster-objects (static-facts *problem*)
+                           (static-predicates *problem*))))
 
 @export
-(defun best-abstract-components (problem)
+(defun best-abstract-components (*problem*)
   (first
-   (sort (abstract-components problem)
+   (sort (abstract-components *problem*)
          #'>
          :key (lambda (vector)
                 (reduce #'* (map 'list #'length vector))))))
