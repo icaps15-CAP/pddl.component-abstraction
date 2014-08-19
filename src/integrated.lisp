4055f550e86bdef58d31f6cbb6d588877e3adb40
Masataro Asai <guicho2.71828@gmail.com>
2014-08-19 19:05:54 +0900

    documentation and verbosity

--- src/integrated.lisp
+++ src/integrated.lisp
@@ -9,27 +9,28 @@
 
 @export
 (defun abstract-components-with-seed (*problem* seed-type)
-  (categorize-by-equality
-   (let* ((sfs (static-facts *problem*))
-          (static-objects (%static-objects sfs))
-          (fluent-objects
-           (set-difference (objects *problem*) static-objects)))
-     (concatenate
-      'vector
-      (map 'vector
-       (lambda (o)
-         (make-abstract-component :components (list o)
-                                  :seed o))
-       fluent-objects)
-      (cluster-objects-with-seed
-       (query-type (domain *problem*) seed-type)
-       (remove seed-type (%all-types static-objects))
-       static-objects
-       sfs
-       (static-predicates *problem*))))
-   #'abstract-type= :transitive nil))
-
-
+  (let* ((sfs (static-facts *problem*))
+         (static-objects (%static-objects sfs))
+         (fluent-objects
+          (set-difference (objects *problem*) static-objects))
+         (acs
+          (concatenate
+           'vector
+           ;; adding objects that do not appear in the static graph and
+           ;; ensure all objects in the domain are included in the result
+           (map 'vector
+                (lambda (o)
+                  (make-abstract-component
+                   :seed o :components (list o)))
+                fluent-objects)
+           (cluster-objects-with-seed
+            (query-type (domain *problem*) seed-type)
+            (remove seed-type (%all-types static-objects))
+            static-objects
+            sfs
+            (static-predicates *problem*)))))
+    (format t "~&~a abstract components, including junks" (length acs))
+    (categorize-by-equality acs #'abstract-type= :transitive nil)))
 
 @export
 (defun best-abstract-components (*problem*)
